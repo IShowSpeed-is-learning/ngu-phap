44a7eef145be774398b1cfd1e75dd1780dace2f3
IShowSpeed-is-learning <binhttg15+github@gmail.com>
2025-10-22 11:31:23 +0700

    Add license and copyright to script.js
    Added license information and copyright notice.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file is part of ngu-phap.
+ *
+ * Copyright (c) 2025 IShowSpeed-is-learning
+ */
+
 // ======= CONFIG =======
 const openAIEndpoint = "https://api.openai.com/v1/chat/completions";
 const geminiEndpoint = "https://generativelanguage.googleapis.com/v1beta/models";
